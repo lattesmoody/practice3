25573e79310f3932497f12086318674073ad66f3
lattesmoody <lattesmoody@gmail.com>
2022-07-05 00:12:32 +0900

    first commit

---
+++ 220704-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world!\n");
+    int a = 0;
+    int b = 2;
+    int c = 0;
+    b = b+3;
+    a = a+1;
+    return 0;
+}
---
+++ 220704-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 4;
+    int b = 3;
+    int c = a + b;
+
+    cout << a << " + " << b << " = " << c << endl;
+
+    return 0;
+}
